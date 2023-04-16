1568cab7a0b856485ce7a12216ce7908c8f2086a
muqianyu <muqian@mail.ru>
2023-04-16 23:57:37 +0300

    First iteration

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd FancyFarm
+pip install virtualenv
+virtualenv venv
+source venv/bin/activate
+python -m pip install "kivy[base]" kivy_examples --no-binary kivy
+python3 FancyFarm.py
+
